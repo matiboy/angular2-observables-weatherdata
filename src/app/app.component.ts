b30b507a711bfb449962907e7f8b0666f7b908ba
matiboy <mathieu.chauvinc@gmail.com>
2016-07-24 17:42:33 +0800

    Using events on buttons instead of (click) in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import {Observable, Subject} from 'rxjs/Rx';
 import {WeatherDataService, WeatherDataInterface} from './weather-data';
 import * as moment from 'moment';
@@ -13,15 +13,20 @@ import {MomentedDatePipe} from './date.pipe';
   pipes: [MomentedDatePipe]
 })
 export class AppComponent implements AfterViewInit {
+  @ViewChild('previous') previous:ElementRef;
+  @ViewChild('next') next:ElementRef;
   dayChanges$ = new Subject<number>();
   daysFromToday$ = this.dayChanges$.scan((ov, nv) => ov + nv, 0);
   currentDate$ = this.daysFromToday$.map(n => moment().add(n, 'days'));
   previousDate$ = this.currentDate$.map(m => m.clone().subtract(1, 'days'));
   nextDate$ = this.currentDate$.map(m => m.clone().add(1, 'days'));
   weatherData$ = this.currentDate$.switchMap(m => this.weatherData.loadData(m)).do(x=>console.log('In app', x)).share();
-  constructor(private weatherData:WeatherDataService) {}
+  constructor(private weatherData:WeatherDataService) {
+  }
 
   ngAfterViewInit() {
-    this.dayChanges$.next(0);
+    let previousButtonClicks$ = Observable.fromEvent(this.previous.nativeElement, 'click').map(_ => -1);
+    let nextButtonClicks$ = Observable.fromEvent(this.next.nativeElement, 'click').map(_ => 1);
+    Observable.concat(previousButtonClicks$, nextButtonClicks$).startWith(0).subscribe(this.dayChanges$);
   }
 }
