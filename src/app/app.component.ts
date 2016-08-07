dbccda412bb041fd05fb208f1f1fb0b0f3c68991
matiboy <mathieu.chauvinc@gmail.com>
2016-08-07 12:26:31 +0800

    Add animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit,trigger,
+  state,
+  style,
+  transition,
+  animate } from '@angular/core';
 import {Observable, Subject} from 'rxjs/Rx';
 import {WeatherDataService, WeatherDataInterface} from './weather-data';
 import * as moment from 'moment';
@@ -10,23 +14,35 @@ import {MomentedDatePipe} from './date.pipe';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
   providers: [WeatherDataService],
-  pipes: [MomentedDatePipe]
+  pipes: [MomentedDatePipe],
+  animations: [
+    trigger('loadingState', [
+      state('notloading', style({
+        opacity: '1'
+      })),
+      state('loading',   style({
+        opacity: '0.3'
+      })),
+      transition('loading <=> notloading', animate('150ms'))
+    ])
+  ]
 })
 export class AppComponent implements AfterViewInit {
   dayChanges$ = new Subject<number>();
-  loading$ = new Subject<boolean>();
   daysFromToday$ = this.dayChanges$.scan((ov, nv) => ov + nv, 0);
   currentDate$ = this.daysFromToday$.map(n => moment().add(n, 'days'));
   previousDate$ = this.currentDate$.map(m => m.clone().subtract(1, 'days'));
   nextDate$ = this.currentDate$.map(m => m.clone().add(1, 'days'));
   weatherData$ = this.currentDate$.switchMap(m => this.weatherData.loadData(m)).do(x=>console.log('In app', x)).share();
-  constructor(private weatherData:WeatherDataService) {}
-
-  ngAfterViewInit() {
-    Observable.merge(
+  loading$ = Observable.merge(
       this.dayChanges$.map(_ => true),
       this.weatherData$.map(_ => false)
-    ).subscribe(this.loading$);
+  );
+  hidden$ = this.loading$.map(b => b ? 'loading' : 'notloading');
+  constructor(private weatherData:WeatherDataService) {
+  }
+
+  ngAfterViewInit() {
     this.dayChanges$.next(0);
   }
 }
